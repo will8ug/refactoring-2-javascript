7b352b3e293a1fa586d4d99a2b2db52ca796fa8d
Will <yzj_000@163.com>
2024-03-06 21:28:53 +0800

    Step 3: Replace temp with query

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function statement(invoice, plays) {
     }).format;
 
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
+        const play = playFor(perf);
         let thisAmount = amountFor(play, perf);
 
         // add volume credits
@@ -26,6 +26,10 @@ function statement(invoice, plays) {
     result += `Amount owed is ${format(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
 
     function amountFor(play, aPerformance) {
         let result = 0;
