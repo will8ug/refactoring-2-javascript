c2ea0978fc6ee2df69fafaaeace55b6bb6e4174f
Will <yzj_000@163.com>
2024-03-07 21:43:00 +0800

    Step 12: Split phase
    Prepare to split it to 2 phases:
    1. Calculate invoice data;
    2. Render invoice data.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-
 function statement(invoice, plays) {
+    const statementData = {};
+    return renderPlainText(statementData, invoice, plays);
+}
+
+function renderPlainText(data, invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     
     for (let perf of invoice.performances) {
-        // print line for this order
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
     
