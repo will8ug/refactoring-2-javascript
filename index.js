f9c8dc1b69cc6dbd078754b4d4b1a06f74e97ea4
Will <yzj_000@163.com>
2024-03-07 22:27:33 +0800

    Step 15: Move volumeCreditsFor() to the method of calculating data

--- index.js
+++ index.js
@@ -8,11 +8,19 @@ function statement(invoice, plays) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
+    }
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+        return result;
     }
 
     function amountFor(aPerformance) {
@@ -60,7 +68,7 @@ function renderPlainText(data) {
     function totalVolumeCredits() {
         let result = 0;
         for (let perf of data.performances) {
-            result += volumeCreditsFor(perf);
+            result += perf.volumeCredits;
         }
         return result;
     }
@@ -71,13 +79,6 @@ function renderPlainText(data) {
             currency: "USD",
             minimumFractionDigits: 2
         }).format(aNumber/100);
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-        return result;
     }
 }
 
