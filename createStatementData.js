c97737586c4c7f3aac470be3e380edad1119f82d
Will <yzj_000@163.com>
2024-04-05 23:56:52 +0800

    1.8 - Step 21: Introduce a new Class PerformanceCalculator

--- createStatementData.js
+++ createStatementData.js
@@ -1,3 +1,10 @@
+class PerformanceCalculator {
+    constructor(aPerformance, aPlay) {
+        this.performance = aPerformance;
+        this.play = aPlay;
+    }
+}
+
 export default function createStatementData(invoice, plays) {
     const sttData = {};
     sttData.customer = invoice.customer;
@@ -7,8 +14,9 @@ export default function createStatementData(invoice, plays) {
     return sttData;
 
     function enrichPerformance(aPerformance) {
+        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
+        result.play = calculator.play;
         result.amount = amountFor(result);
         result.volumeCredits = volumeCreditsFor(result);
         return result;
